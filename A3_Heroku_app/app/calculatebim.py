2eb64837741c364b364d1e67d3a08ebd428f6bde
sofia bourhim <=>
2022-01-02 10:25:59 +0100

    Assignement 3: Heroku App

---
+++ A3_Heroku_app/app/calculatebim.py
@@ -0,0 +1,7 @@
+def calculate_bmi(weight,height):
+    """
+    Calculate the BMI of the individual
+    """
+    bmi = weight / (height ** 2)
+    
+    return bmi
---
+++ A3_Heroku_app/app/flaskapp.py
@@ -0,0 +1,23 @@
+from app.calculatebim import calculate_bmi
+from flask import Flask, app, jsonify, request
+
+#Instantiate flask object
+app =  Flask('__name__')
+
+@app.route('/', methods = ['GET','POST'])
+def get_input():
+    """
+    A function to get request using flask, evaluate and return result
+    """
+    packet = request.get_json(force=True)
+    weight = packet["weight"]
+    height = packet["height"]
+
+    bmi = calculate_bmi(weight,height)
+
+    return jsonify(packet,bmi)
+
+# driver function to run the app
+
+if __name__  == '__main__':
+    app.run(debug = True)
